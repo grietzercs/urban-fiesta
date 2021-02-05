a7663d6c151a02336d6d3adbef4f4c65557241f0
Colden <grietzercs@vcu.edu>
2021-02-05 17:23:09 -0500

    added a lot

---
+++ testCode/plusplus.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main(void) {
+    std::cout << "Enter your favorite number\n";
+    printf("Test\n");
+}
---
+++ testCode/test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+    printf("Hello World\n");
+
+    int five = 5;
+    int three = 3;
+    int sum = five + three;
+    
+    printf("Addition: %d + %d = %d\n", five, three, sum);
+    return 0;
+}
