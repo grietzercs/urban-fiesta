c1954f2a0937cc9a49ab119fcf76f2c09645a442
Colden <grietzercs@vcu.edu>
2021-03-26 04:19:10 -0400

    assignment 2 part 1 finished

--- assignment2/reader-writer.c
+++ assignment2/reader-writer.c
@@ -6,11 +6,15 @@
 This program provides a possible solution for first readers writers problem using mutex and semaphore.
 I have used 10 readers and 5 producers to demonstrate the solution. You can always play with these values.
 */
+#define numCount 5
 
 sem_t wrt;
 pthread_mutex_t mutex;
 int cnt = 1;
 int numreader = 0;
+sem_t countSem;
+
+int semValue;
 
 void *writer(void *wno)
 {   
@@ -24,6 +28,17 @@ void *reader(void *rno)
 {   
     // Reader acquire the lock before modifying numreader
     pthread_mutex_lock(&mutex);
+
+    int test = sem_trywait(&countSem);
+    if(test == 0) {
+        sem_post(&countSem);
+    } else {
+        printf("semaphore full -----------\n");
+    }
+    sem_wait(&countSem);
+    sem_getvalue(&countSem, &semValue);
+    printf("countSem_wait value: %d\n", semValue);
+
     numreader++;
     if(numreader == 1) {
         sem_wait(&wrt); // If this id the first reader, then it will block the writer
@@ -35,6 +50,11 @@ void *reader(void *rno)
     // Reader acquire the lock before modifying numreader
     pthread_mutex_lock(&mutex);
     numreader--;
+    
+    sem_post(&countSem);
+    sem_getvalue(&countSem, &semValue);
+    printf("countSem_post value: %d\n", semValue);
+    
     if(numreader == 0) {
         sem_post(&wrt); // If this is the last reader, it will wake up the writer.
     }
@@ -47,6 +67,10 @@ int main()
     pthread_t read[10],write[5];
     pthread_mutex_init(&mutex, NULL);
     sem_init(&wrt,0,1);
+
+    sem_init(&countSem, 0, numCount);
+    // sem_getvalue(&countSem, &semValue);
+    // printf("countSem_init value: %d\n", semValue);
 
     int a[10] = {1,2,3,4,5,6,7,8,9,10}; //Just used for numbering the producer and consumer
 
